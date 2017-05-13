bdb259f27331a29911c74c2f51f9d042bbba5db2
Brandon1234 <brandonlit@gmail.com>
2017-05-12 20:14:18 -0400

    Class, and text file creation
    The text file has 2 test questions in the format they should be read in.
    The java class is for the MC questions and have both constructors made
    but lacking in code

---
+++ src/projectmanagementproject/MultipleChoice.java
@@ -0,0 +1,26 @@
+/* Brandon Lit
+ * 2017-05-12
+ * This is the multiple choice class which will be used for all questions*/
+package projectmanagementproject;
+
+public class MultipleChoice {
+    /**
+     * The constructor for regular multiple choice programs
+     * @param question
+     * @param andA
+     * @param ansB
+     * @param ansC
+     * @param ansD 
+     */
+    public MultipleChoice(String question, String andA, String ansB, String ansC, String ansD){
+        
+    }
+    /**
+     * The constructor for true false questions
+     * @param question
+     * @param ans 
+     */
+    public MultipleChoice(String question, boolean ans){
+        
+    }
+}
