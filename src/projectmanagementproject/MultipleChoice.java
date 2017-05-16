cf0a2b9509507b49a492373d9745372214f13d33
dhav8r <grummanf8f@gmail.com>
2017-05-15 22:09:59 -0400

    set up accessors for the multiple choice class also made clone equals and toString methods

--- src/projectmanagementproject/MultipleChoice.java
+++ src/projectmanagementproject/MultipleChoice.java
@@ -35,4 +35,110 @@ public class MultipleChoice {
         trueFalseAns = ans;
         isTrueFalse = true;
     }
+    /**
+     * accessor for the question
+     * @return the question
+     */
+    public String getQuestion(){
+        return question;
+    }
+    /**
+     * accessor for option a
+     * @return option a
+     */
+    public String getAnswerA(){
+        return ansA;
+    }
+    /**
+     * accessor for option b
+     * @return option b
+     */
+    public String getAnswerB(){
+        return ansB;
+    }
+    /**
+     * accessor for option c
+     * @return option c
+     */
+    public String getAnswerC(){
+        return ansC;
+    }
+    /**
+     * accessor for option d
+     * @return option d
+     */
+    public String getAnswerD(){
+        return ansD;
+    }
+    /**
+     * accessor to get the correct answer to a multiple choice question
+     * @return a string containing a, b, c, or d
+     */
+    public String getMCAnswer(){
+        return correct;
+    }
+    /**
+     * accessor for the answer if the question is true or false;
+     * @return true or false, depending on the answer
+     */
+    public boolean getTFAnswer(){
+        return trueFalseAns;
+    }
+    /**
+     * Displays all of the information contained in a question
+     * @return a string with the relevant info
+     */
+    @Override
+    public String toString(){
+        String s;
+        //check if the question is true or false
+        if(!isTrueFalse){
+            //if it isnt display the different options and the answer
+            s = "Question: " + question 
+                    + "\na) " + ansA 
+                    + "\nb) " + ansB
+                    + "\nc) " + ansC
+                    + "\nd) " + ansD
+                    + "Answer: " + correct;
+        }else{
+            //if it is, show the question and true or false, along with the answer
+            s = "Question: " + question 
+                    + "a) True\nb) False"
+                    + "Answer: " + trueFalseAns;
+        }
+        return s;
+    }
+    /**
+     * make a new multiple choice question identical to this one
+     * @return the new question
+     */
+    public MultipleChoice clone(){
+        //if it is true or false
+        if(isTrueFalse){
+            //use the correct constructor
+            return new MultipleChoice(question, trueFalseAns);
+        }else{
+            //if not use the other constructor
+            return new MultipleChoice(question, ansA, ansB, ansC, ansD, correct);
+        }
+    }
+    /**
+     * equals method to check if to questions are the same
+     * @param q takes another question
+     * @return true or false, depending on whether or not the questions are the same
+     */
+    public boolean equals(MultipleChoice q){
+        //if the questions are the same
+        if(q.getQuestion().equals(question)){
+            //see if they also have the same answer
+            if(isTrueFalse && q.getTFAnswer() == trueFalseAns){
+                //if they do, return true
+                return true;
+            }else if(q.getMCAnswer().equals(correct)){
+                return true;
+            }
+        }
+        //if they aren't the same, return false
+        return false;
+    }
 }
